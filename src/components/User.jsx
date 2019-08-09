d812791b830122152caeac0422e9137f4b9e2d45
AHouy <alexander.houy@utexas.edu>
2019-08-09 00:47:37 -0500

    Close #3, #4

---
+++ src/components/User.jsx
@@ -0,0 +1,468 @@
+import React from "react";
+import DatePicker from "react-datepicker";
+
+export default class User extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoaded: false,
+      spotifyUser: {
+        accessToken: localStorage.getItem("spotify_access_token"),
+        authState: localStorage.getItem("spotify_auth_state")
+      },
+      user: { images: [{ url: null }] },
+      location: "",
+      locations: [],
+      city: "",
+      date: new Date()
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchUser();
+    this.fetchLocation();
+  }
+
+  handleSubmit(event) {
+    this.fetchLocation();
+    event.preventDefault();
+  }
+
+  async fetchUser() {
+    const data = await fetch("https://api.spotify.com/v1/me/", {
+      headers: {
+        Authorization: `Bearer ${this.state.spotifyUser.accessToken}`
+      }
+    }).then(res => res.json());
+    if (data.error) {
+      localStorage.removeItem("spotify_access_token");
+      window.location.href = "/";
+    }
+    this.setState({ isLoaded: true, user: data });
+    console.log(data);
+  }
+
+  async fetchLocation() {
+    const data = await fetch(
+      `/location?q=${this.state.location.toLowerCase()}`
+    ).then(res => res.json());
+    this.setState(state => {
+      let result = { locations: data };
+      if (data.length > 0) result.city = data[0].metroArea.id;
+      else result.city = "";
+      return result;
+    });
+  }
+
+  renderUser() {
+    return (
+      <div className="media">
+        <img
+          className="mr-3"
+          width="150"
+          src={
+            this.state.user.images.length > 0
+              ? this.state.user.images[0].url
+              : ""
+          }
+        />
+        <div className="media-body">
+          <h5 className="mt-0">Logged in as {this.state.user.display_name}</h5>
+        </div>
+      </div>
+    );
+  }
+
+  renderLocationSearch() {
+    return (
+      <form>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="City"
+            value={this.state.location}
+            onChange={event => this.setState({ location: event.target.value })}
+          />
+          <div className="input-group-append">
+            <button
+              className="btn btn-dark"
+              type="submit"
+              onClick={this.handleSubmit}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </form>
+    );
+  }
+
+  renderLocations() {
+    if (this.state.locations.length > 0)
+      return (
+        <select
+          className="form-control"
+          value={this.state.city}
+          onChange={event => this.setState({ city: event.target.value })}
+        >
+          {this.state.locations.map(location => {
+            let metroArea = location.metroArea;
+            let display;
+            if (metroArea.state)
+              display = `${metroArea.displayName}, ${
+                metroArea.state.displayName
+              }, ${metroArea.country.displayName}`;
+            else
+              display = `${metroArea.displayName}, ${
+                metroArea.country.displayName
+              }`;
+            return <option value={location.metroArea.id}>{display}</option>;
+          })}
+        </select>
+      );
+    else
+      return (
+        <select className="form-control" value={0}>
+          <option value={0}>No locations found.</option>
+        </select>
+      );
+  }
+
+  renderDatePicker() {
+    return (
+      <div class="form-group">
+        <DatePicker
+          className="form-control"
+          selected={this.state.date}
+          placeholderText="Select a date"
+          popperPlacement="top-end"
+          onChange={date => this.setState({ date: date })}
+          popperModifiers={{
+            offset: {
+              enabled: true,
+              offset: "5px, 10px"
+            },
+            preventOverflow: {
+              enabled: true,
+              escapeWithReference: false, // force popper to stay in viewport (even when input is scrolled out of view)
+              boundariesElement: "viewport"
+            }
+          }}
+        />
+      </div>
+    );
+  }
+
+  render() {
+    if (this.state.isLoaded)
+      return (
+        <React.Fragment>
+          <div id="loggedin">
+            <div id="user-profile">{this.renderUser()}</div>
+          </div>
+          <div className="row mt-3 mb-3">
+            <div className="col-lg-8">
+              <div className="row mb-3">
+                <div className="col-md-6">{this.renderLocationSearch()}</div>
+                <div className="col-md-6">{this.renderLocations()}</div>
+              </div>
+            </div>
+            <div className="col-lg-4">{this.renderDatePicker()}</div>
+          </div>
+          <Concerts
+            city={this.state.city}
+            user={this.state.user}
+            date={this.state.date.toISOString().split("T")[0]}
+          />
+        </React.Fragment>
+      );
+    else return <div className="loader" />;
+  }
+}
+
+class Concerts extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { data: [], isLoading: false };
+  }
+
+  async fetchConcert(endpt) {
+    if (this.props.city) {
+      if (!endpt)
+        endpt = `/bandstonight?location=${this.props.city}&date=${
+          this.props.date
+        }`;
+      const data = await fetch(endpt).then(res => res.json());
+      this.setState(state => {
+        return {
+          data: state.data.concat(
+            data.results.sort((a, b) => {
+              let x = a.event.start.time;
+              let y = b.event.start.time;
+              if (x < y) {
+                return -1;
+              }
+              if (x > y) {
+                return 1;
+              }
+              return 0;
+            })
+          )
+        };
+      });
+      if (data.next) this.fetchConcert(data.next);
+      else this.setState({ isLoading: false });
+    }
+  }
+
+  componentDidMount() {
+    this.fetchConcert();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (
+      this.props.city !== prevProps.city ||
+      this.props.date !== prevProps.date
+    ) {
+      this.setState({ isLoading: true, data: [] });
+      this.fetchConcert();
+    }
+  }
+
+  render() {
+    let loader;
+    if (this.state.isLoading)
+      loader = (
+        <div className="mt-2 mb-2">
+          <h4 className="text-muted text-center">
+            Getting {this.state.data.length > 0 ? "more of " : ""}tonight's
+            concerts and bands...
+          </h4>
+          <div className="loader mx-auto" />
+        </div>
+      );
+    let stateOrCountry, location;
+    if (this.state.data.length > 0) {
+      stateOrCountry =
+        this.state.data[0].event.venue.metroArea.state ||
+        this.state.data[0].event.venue.metroArea.country;
+      location =
+        this.state.data[0].event.venue.metroArea.displayName +
+        ", " +
+        stateOrCountry.displayName;
+    } else if (this.props.city !== "" && !this.state.isLoading) {
+      return (
+        <p className="text-muted text-center">
+          No concerts found in this city.
+        </p>
+      );
+    } else if (!this.props.city)
+      return <p className="text-muted text-center">Search for a city.</p>;
+    return (
+      <React.Fragment>
+        {loader
+          ? loader
+          : this.state.data.length > 0 && (
+              <SpotifyPlaylistGenerator
+                user={this.props.user}
+                location={location}
+                date={
+                  this.state.data.length !== 0
+                    ? this.state.data[0].event.start.date
+                    : ""
+                }
+              />
+            )}
+        {this.state.data.map(data => (
+          <ConcertInfo artists={data.artists} event={data.event} />
+        ))}
+      </React.Fragment>
+    );
+  }
+}
+
+function ConcertInfo(props) {
+  return (
+    <ul className="list-unstyled" style={{ width: "100%" }}>
+      <li className="media">
+        <div className="media-body">
+          <h5 className="mt-3">
+            <a href={props.event.uri} target="_blank">
+              {props.event.displayName}
+              {props.event.start.time ? " @ " + props.event.start.time : ""}
+            </a>
+          </h5>
+          <div>
+            {props.artists.map(artist => (
+              <Artist artist={artist} key={artist.id} />
+            ))}
+          </div>
+        </div>
+      </li>
+    </ul>
+  );
+}
+
+class Artist extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      include: true
+    };
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  handleButtonClick() {
+    this.setState(state => ({ include: !state.include }));
+  }
+
+  render() {
+    if (this.props.artist.spotify)
+      return (
+        <div className="row">
+          <div className="col-md-8">
+            <div
+              className={
+                this.state.include ? "media mt-3 artist" : "media mt-3"
+              }
+              dataSpotifyId={this.props.artist.id}
+            >
+              <img
+                className="mr-3"
+                src={this.props.artist.image}
+                alt={this.props.artist.name}
+                width="100px"
+              />
+              <div className="media-body">
+                <h5 className="mt-0">{this.props.artist.name}</h5>
+                <p className="text-muted">
+                  <b>{this.props.artist.genres.join(", ")}</b>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="mt-3 align-self-center col-md-4">
+            <button
+              type="button"
+              className={
+                this.state.include
+                  ? "btn btn-danger btn-block"
+                  : "btn btn-success btn-block"
+              }
+              onClick={this.handleButtonClick}
+            >
+              {this.state.include ? "Remove from Playlist" : "Add to Playlist"}
+            </button>
+          </div>
+        </div>
+      );
+    else
+      return (
+        <div className="media mt-3">
+          <img
+            className="mr-3"
+            src="https://via.placeholder.com/100"
+            alt={this.props.artist.name}
+            width="100px"
+          />
+          <div className="media-body">
+            <h5 className="mt-0">
+              {this.props.artist.name} -{" "}
+              <span className="text-danger">Could not find on Spotify</span>
+            </h5>
+          </div>
+        </div>
+      );
+  }
+}
+
+class SpotifyPlaylistGenerator extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      spotifyUser: {
+        accessToken: localStorage.getItem("spotify_access_token"),
+        authState: localStorage.getItem("spotify_auth_state")
+      },
+      playlist: null,
+      isLoading: false
+    };
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  componentDidMount() {
+    console.log(this.props.user);
+  }
+
+  handleButtonClick() {
+    this.setState({ isLoading: true });
+    // Get each artist's spotify id
+    let artistSpotifyIds = [];
+
+    let artists = document.getElementsByClassName("artist");
+    for (let artist of artists) {
+      let spotifyId = artist.attributes.dataspotifyid.value;
+      artistSpotifyIds.push(spotifyId);
+    }
+
+    // Generate the playlist
+    fetch("/playlist", {
+      method: "post",
+      body: JSON.stringify({
+        accessToken: this.state.spotifyUser.accessToken,
+        userId: this.props.user.id,
+        location: this.props.location,
+        date: this.props.date,
+        artistSpotifyIds: artistSpotifyIds
+      })
+    })
+      .then(res => res.json())
+      .then(data => this.setState({ playlist: data.id, isLoading: false }));
+  }
+
+  render() {
+    let button = (
+      <button
+        type="button"
+        onClick={this.handleButtonClick}
+        className="btn btn-secondary btn-lg btn-block"
+      >
+        Generate {this.state.playlist ? "another " : ""}Playlist!
+      </button>
+    );
+    if (this.state.isLoading)
+      return (
+        <div className="mt-2 mb-2">
+          <h4 className="text-muted text-center">
+            Generating {this.state.playlist ? "another " : ""}playlist...
+          </h4>
+          <div className="loader mx-auto" />
+        </div>
+      );
+    if (this.state.playlist)
+      return (
+        <React.Fragment>
+          {button}
+          <div className="mx-auto mt-2 mb-2" style={{ width: "300px" }}>
+            <iframe
+              src={`https://open.spotify.com/embed/playlist/${
+                this.state.playlist
+              }`}
+              width="300"
+              height="380"
+              frameborder="0"
+              allowtransparency="true"
+              allow="encrypted-media"
+            />
+          </div>
+        </React.Fragment>
+      );
+    return button;
+  }
+}
